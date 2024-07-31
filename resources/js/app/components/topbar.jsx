4c0b6f9365988de0e1cf3ffc9b72d5da185591c5
Emman <click2edit@gmail.com>
2024-07-31 22:36:48 +0800

    topbar revised

--- resources/js/app/components/topbar.jsx
+++ resources/js/app/components/topbar.jsx
@@ -115,7 +115,6 @@ export default function Topbar() {
                                         )}
                                     </MenuItem>
                                 ))}
-                                {/* <LogoutButton/> */}
                             </MenuItems>
                         </Menu>
                     </div>
